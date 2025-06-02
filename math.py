b7c91cd6657a7762676c0e43eb5bc263f46599e4
Ethan Stanks <estanks@revspringinc.com>
2025-06-02 16:30:13 -0500

    Creating initial demo file.

---
+++ math.py
@@ -0,0 +1,13 @@
+
+def add(a, b):
+    """Returns the sum of a and b."""
+    return a + b
+
+def subtract(a, b):
+    """Returns the difference of a and b."""
+    return a - b
+
+
+if __name__ == "__main__":
+    print(f"Add: {add(5, 3)}")
+    print(f"Subtract: {subtract(5, 3)}")
